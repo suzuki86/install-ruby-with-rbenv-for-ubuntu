0e852bd51155676d6e0209c9fafb3aa5cb26376d
suzuki86 <tsnr0001@gmail.com>
2014-06-08 13:37:55 +0900

    Fisrt commit

---
+++ install_ruby_with_rbenv_for_ubuntu.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Install libssl
+sudo apt-get install libssl-dev
+
+# Install rbenv
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+source ~/.bash_profile
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+# Install ruby
+rbenv install 2.1.2
+rbenv rehash
+rbenv global 2.1.2
+
